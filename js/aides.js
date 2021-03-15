b884315e415de50e078b3a4363b05c484fa1c4e5
karen-laporta <kyoung2007@gmail.com>
2021-03-15 12:15:13 -0400

    Update aides.js

--- js/aides.js
+++ js/aides.js
@@ -17,4 +17,31 @@ function repEmail_BH_ASM(Code){
 	document.asmEmail.submit();
 }
 
+const setupSelectToReveal = (selectDropdown, toggleClass) => {
+    const selectCommittee = document.getElementById(selectDropdown);
+	 
+	selectCommittee.addEventListener("change", function() {
+        
+			if(selectCommittee.value != "Select a Committee"){
+				let committee = document.getElementById(selectCommittee.value);
+				closeOpenElements(toggleClass);
+				committee.classList.toggle(toggleClass);
+			}
+	    });
+}
+
+setupSelectToReveal("assemblyCommittee", "committee__show");
+setupSelectToReveal("senateCommittee", "committee__show");
+
+ 
+function showSenateCommittee() {
+	const senate = document.getElementById('senate');
+	closeOpenElements("assemblyShow");
+	senate.classList.toggle("senateShow");
+} 
+function showAssemblyCommittee() {
+	const assembly = document.getElementById('assembly');
+	closeOpenElements("senateShow");
+	assembly.classList.toggle("assemblyShow");
+}
 
